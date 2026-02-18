ff05a87cb6d74fc1dd51ee322d85e639db62d348
Vaishnavi Dharmadhikari <vaishnavidharmadhikari64@gmail.com>
2026-02-18 11:55:55 +0530

    Add InputUtil with methods to perform input/output

--- src/main/java/com/deskmate/utils/InputUtil.java
+++ src/main/java/com/deskmate/utils/InputUtil.java
@@ -1,5 +1,19 @@
 package com.deskmate.utils;
 
-public class InputUtil {
+import java.util.Scanner;
 
+public final class InputUtil {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private InputUtil() {}
+
+    public static int readInt(String prompt) {
+        System.out.print(prompt);
+        return Integer.parseInt(sc.nextLine().trim());
+    }
+
+    public static String readString(String prompt) {
+        System.out.print(prompt);
+        return sc.nextLine().trim();
+    }
 }

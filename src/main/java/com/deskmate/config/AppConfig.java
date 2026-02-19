b02bcb621797f4c2fe344d2b64505549c346b672
Vaishnavi Dharmadhikari <vaishnavidharmadhikari64@gmail.com>
2026-02-19 08:55:49 +0530

    Update some changes in app.config

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,8 +1,14 @@
 package com.deskmate.config;
 
+import com.deskmate.controller.BookingController;
 import com.deskmate.controller.DeskController;
+import com.deskmate.dao.BookingDao;
 import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.JdbcBookingDao;
 import com.deskmate.dao.JdbcDeskDao;
+import com.deskmate.dao.JdbcPaymentDao;
+import com.deskmate.dao.PaymentDao;
+import com.deskmate.services.BookingService;
 import com.deskmate.services.DeskService;
 
 public class AppConfig {
@@ -13,14 +19,14 @@ public class AppConfig {
 	        return new DeskController(deskService);
 	    }
 
-//	    public BookingController bookingController() {
-//	        DeskDao deskDao = new JdbcDeskDao();
-//	        BookingDao bookingDao = new JdbcBookingDao();
-//	        PaymentDao paymentDao = new JdbcPaymentDao();
-//	        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
-//	        return new BookingController(bookingService);
-//	    }
-//
+	    public BookingController bookingController() {
+	        DeskDao deskDao = new JdbcDeskDao();
+	        BookingDao bookingDao = new JdbcBookingDao();
+	        PaymentDao paymentDao = new JdbcPaymentDao();
+	        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
+	        return new BookingController(bookingService);
+	    }
+
 //	    public ReportController reportController() {
 //	        ReportDao reportDao = new JdbcReportDao();
 //	        ReportService reportService = new ReportService(reportDao);
